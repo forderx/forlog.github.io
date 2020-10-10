4679dc34833232d5cd026e5ee08ef05ab64eccd0
forlog <63668848+for-log@users.noreply.github.com>
2020-10-10 18:31:55 +0500

    Update js.js

--- js.js
+++ js.js
@@ -15,9 +15,14 @@ $(document).ready(function() {
     $(".about").on('click', ()=>{
         $(".minlanginfo").css({'right':"-100%"})
         $(".aboutinfo").css({'left':'0%'})
-    })
+    });
     $(".lang").on('click', ()=>{
         $(".aboutinfo").css({'left':'-100%'})
         $(".minlanginfo").css({'right':"0%"})
-    })
+    });
+    $(".logo").on('click', ()=>{
+        $('.aboutinfo').css({'height':'100%'});
+        $('.mininfo').css({'height':'100%'});
+        $(".minlanginfo").css({"height":'100%'});
+    });
 });
