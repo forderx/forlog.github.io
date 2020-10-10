7c6c512d52684ada54789c4ffbe4594411713952
forlog <63668848+for-log@users.noreply.github.com>
2020-10-10 16:15:26 +0500

    Add files via upload
    new files(with slider)

---
+++ js.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1){
+            $('.navbar').addClass("whiteTheme");
+            $(".logo").addClass("blackTheme")
+            $(".links a").addClass("blackTheme")
+        } else {
+            $('.navbar').removeClass("whiteTheme");
+            $(".logo").removeClass("blackTheme")
+            $(".links a").removeClass("blackTheme")
+        }
+    });
+
+    $(".about").on('click', ()=>{
+        $(".minlanginfo").css({'right':"-100%"})
+        $(".aboutinfo").css({'left':'0%'})
+    })
+    $(".lang").on('click', ()=>{
+        $(".aboutinfo").css({'left':'-100%'})
+        $(".minlanginfo").css({'right':"0%"})
+    })
+});
---
+++ vue.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      mainpage: 'ForLog | Главная',
+      lengthwork: 1,
+      infoabout: "Меня зовут Давид",
+      minbutaboud: "Обо мне",
+      minbutlang: "Языки программирования",
+      textabout: "Я являюсь веб-разработчиком, пишу сайты на заказ",
+      infolang: "Языки программирования на которых я пишу",
+      textlang: "Я занимаюсь front-end и back-end разработкой, ранее писал на: HTML, CSS, JS, PHP",
+      worknames: ['ForLog'],
+      workinfo: ['Мой проект, мое портфолио, на котором вы сейчас находитесь'],
+      worklinks: ['#'],
+      workimgs: ['https://sun9-19.userapi.com/qXrSUvzOPcJL1W78Sr5YlmRlwlkkpVNxNp270A/UcL5GEseyQE.jpg'],
+    },
+    methods: {
+      // changeMinContent: function (item) {
+      //   if (item == "about") {
+      //     app.img = "https://i.pinimg.com/originals/0c/ac/5e/0cac5ed32498bfe3e707b8492e0db3e0.jpg"
+      //     app.info = "Меня зовут Давид"
+      //     app.text = "Я являюсь веб-разработчиком, пишу сайты на заказ"
+      //   }else {
+      //     app.img = "https://wallpapershome.ru/images/pages/pic_h/20329.jpg"
+      //     app.info = "Языки программирования"
+      //     app.text = "Я занимаюсь front-end и back-end разработкой, ранее писал на: HTML, CSS, JS, PHP"
+      //   }
+      // }
+    }
+
+  })
