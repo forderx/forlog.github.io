a06b33f4114d04e9ef87143cfe0495a7da049c1e
forlog <63668848+for-log@users.noreply.github.com>
2020-10-10 23:49:58 +0500

    Update js.js

--- js.js
+++ js.js
@@ -25,14 +25,11 @@ $(document).ready(function() {
         $(".max").css({'height':$(window).height() + 'px'});
     });
     
-    $(".logo").typed({
-            strings: ["ForLog", "ForDog", "ForGod"],
-            typeSpeed: 70,
-            backDelay: 1500,
-            startDelay: 2500,
-            loop: true,
-            loopCount: 2,
-            contentType: 'html',      
-        });
+    $('.logo').typeIt({
+     strings: ["ForLog.", "ForDog.", "ForGod."],
+     speed: 50,
+     breakLines: false,
+     autoStart: false
+});
     
 });
