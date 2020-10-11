d164e90f5f39050ac97cd6d5ee9ea6e6c3cc00bb
forlog <63668848+for-log@users.noreply.github.com>
2020-10-11 17:59:06 +0500

    Update vue.js

--- vue.js
+++ vue.js
@@ -14,19 +14,6 @@ var app = new Vue({
       workinfo: ['Мой проект, мое портфолио, на котором вы сейчас находитесь'],
       worklinks: ['#'],
       workimgs: ['./for-log.png'],
-    },
-    methods: {
-      // changeMinContent: function (item) {
-      //   if (item == "about") {
-      //     app.img = "https://i.pinimg.com/originals/0c/ac/5e/0cac5ed32498bfe3e707b8492e0db3e0.jpg"
-      //     app.info = "Меня зовут Давид"
-      //     app.text = "Я являюсь веб-разработчиком, пишу сайты на заказ"
-      //   }else {
-      //     app.img = "https://wallpapershome.ru/images/pages/pic_h/20329.jpg"
-      //     app.info = "Языки программирования"
-      //     app.text = "Я занимаюсь front-end и back-end разработкой, ранее писал на: HTML, CSS, JS, PHP"
-      //   }
-      // }
     }
 
   })
