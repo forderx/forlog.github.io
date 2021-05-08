651bb3bd7633a013da122f2a81c44105837b70b6
forlog <63668848+for-log@users.noreply.github.com>
2021-05-08 15:36:18 +0300

    add sites

--- vue.js
+++ vue.js
@@ -2,17 +2,17 @@ var app = new Vue({
     el: '#app',
     data: {
       mainpage: 'ForLog | Главная',
-      lengthwork: 2,
+      lengthwork: 3,
       infoabout: "Приветствую, меня зовут Давид",
       minbutaboud: "Обо мне",
       minbutlang: "Языки программирования",
       textabout: "Я являюсь веб-разработчиком, пишу сайты на заказ",
       infolang: "Языки программирования на которых я пишу",
       textlang: "Я занимаюсь front-end и back-end разработкой, пишу на: ",
-      lang:"HTML, CSS, JS, PHP",
-      worknames: ['ForLog','PostSoc'],
-      workinfo: ['Мой проект, мое портфолио, на котором вы сейчас находитесь', 'Мой проект, социальная сеть'],
-      worklinks: ['#', 'https://postsoc.000webhostapp.com/index'],
+      lang:"HTML, CSS, JS, PHP, JAVA, PYTHON",
+      worknames: ['ForLog','PostSoc', 'ForTube'],
+      workinfo: ['Портфолио, на котором вы сейчас находитесь', 'Социальная сеть', 'Сервис для просмотра видео YOUTUBE с друзьями'],
+      worklinks: ['#', 'https://postsoc.000webhostapp.com/index', 'https://secure-island-66109.herokuapp.com/'],
       workimgs: ['./for-log.png','./postsoc.png'],
     }
 
