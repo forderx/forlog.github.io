f394670b54bece5233c89f10f3108755ee8bd964
forlog <63668848+for-log@users.noreply.github.com>
2021-07-14 21:51:39 +0500

    Update vue.js

--- vue.js
+++ vue.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
       mainpage: 'ForLog | Главная',
-      lengthwork: 3,
+      lengthwork: 4,
       infoabout: "Приветствую, меня зовут Давид",
       minbutaboud: "Обо мне",
       minbutlang: "Языки программирования",
@@ -10,10 +10,10 @@ var app = new Vue({
       infolang: "Языки программирования на которых я пишу",
       textlang: "Я занимаюсь front-end и back-end разработкой, пишу на: ",
       lang:"HTML, CSS, JS, PHP, JAVA, PYTHON",
-      worknames: ['ForLog','PostSoc', 'ForTube'],
-      workinfo: ['Портфолио, на котором вы сейчас находитесь', 'Социальная сеть', 'Сервис для просмотра видео YOUTUBE с друзьями'],
-      worklinks: ['#', 'https://postsoc.000webhostapp.com/index', 'https://secure-island-66109.herokuapp.com/'],
-      workimgs: ['./for-log.png','./postsoc.png', './fortube.png'],
+      worknames: ['ForLog','PostSoc', 'ForTube', 'TicTacToe'],
+      workinfo: ['Портфолио, на котором вы сейчас находитесь', 'Социальная сеть', 'Сервис для просмотра видео YOUTUBE с друзьями', 'Крестики нолики онлайн'],
+      worklinks: ['#', 'https://postsoc.000webhostapp.com/index', 'https://secure-island-66109.herokuapp.com/', 'https://tictactoeforder.herokuapp.com/'],
+      workimgs: ['./for-log.png','./postsoc.png', './fortube.png', './image.png'],
     }
 
   })
